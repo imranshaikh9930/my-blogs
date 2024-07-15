717193dcc3cb0ca61de319d6210965dd557af870
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-07-15 19:45:09 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://my-blogs-frontend.onrender.com",
+    origin: "my-blogs-frontend2.onrender.com",
     credentials: true,
   })
 );
